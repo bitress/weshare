641156bbc97961a991365dd6fa4c8e79d3c79afb
itscyanne <cyannejustinl.vega12@gmail.com>
2022-12-23 14:23:53 +0800

    upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+
+$localhost = "localhost";
+$username = "root";
+$password = "";
+$database = "weshare";
+
+$con = new mysqli($localhost, $username, $password, $database);
+
+if ($con->connect_errno){
+    echo "Error: ". $con->connect_error;
+}
