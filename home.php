b0c74ec47176e9e33a7ed164b04f1dd0e0324de1
ItsCyanne <cyannejustinl.vega12@gmail.com>
2022-12-30 04:51:33 +0800

    update

--- home.php
+++ home.php
@@ -2,15 +2,47 @@
 
 include_once 'includes/connection.php';
 
+if (isset($_SESSION['isLoggedIn'])){
+
+    $id = $_SESSION['id'];
+
+    $sql = "SELECT * FROM users WHERE user_id = '$id'";
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_assoc($result);
+} else {
+    header("Location: index.php");
+}
+
 
 if (isset($_POST['post_button'])){
 
+    $user_id = $row['user_id'];
+    $title = $_POST['title'];
     $content = $_POST['content'];
 
-    $sql = "INSERT INTO posts (user_id, content) VALUES (1, $content)";
+    $sql = "INSERT INTO posts (user_id,title, content) VALUES ('$user_id','$title', '$content')";
+    $result = mysqli_query($con, $sql);
 
+    if ($result === TRUE){
+        header("Location: home.php");
+    }
 }
 
+if (isset($_POST['like_btn'])){
+
+    $user_id = $_POST['user_id'];
+    $post_id = $_POST['post_id'];
+    $likes = $_POST['like_value'];
+
+    $likes += 1;
+
+    $sql = "UPDATE posts SET likes = '$likes' WHERE post_id = '$post_id'";
+    $result = mysqli_query($con, $sql);
+    if ($result === TRUE){
+        header("Location: home.php");
+    }
+
+}
 
 
 ?>
@@ -22,7 +54,6 @@ if (isset($_POST['post_button'])){
     <title>Home | WeShare</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
@@ -59,9 +90,11 @@ if (isset($_POST['post_button'])){
             <div class="card">
                 <div class="card-body">
                     <form method="post" action="<?= htmlentities($_SERVER['PHP_SELF']); ?>">
+                        <div class="form-group mb-3"></div>
+                        <input type="text" name="title" class="form-control" placeholder="Enter your post title">
                         <div class="form-group mb-3">
-                            <label>What is going on?</label>
-                            <textarea class="form-control" name="content" rows="5"></textarea>
+                            <label>Text (Optional)</label>
+                            <textarea class="form-control" name="content" rows="5" placeholder=""></textarea>
                         </div>
                         <div class="col-md-3">
                             <button type="submit" name="post_button" class="btn btn-success">Post</button>
@@ -70,19 +103,36 @@ if (isset($_POST['post_button'])){
                 </div>
             </div>
 
+            <?php
+
+            $sql = "SELECT posts.*, users.username FROM posts LEFT JOIN users ON users.user_id = posts.post_id ORDER BY date_created";
+            $result = mysqli_query($con, $sql);
+            while ($res = mysqli_fetch_assoc($result)){
+
+            ?>
 
             <!--Post Card-->
             <div class="animate__animated animate bounce card mt-3">
                 <div class="card-body">
-                    <h5 class="card-title ms-1">Sample Post</h5>
-                    <p class="card-text mb-5 ms-1">See more bootstrap code snippets on my website</p>
-
-                    <a href="https://freesnippetcode.blogspot.com/" target="_blank" class="btn btn-primary mb-1 mt-1 ">Go now</a>
+                    <h5 class="card-title ms-1"><?php echo (!empty($res['title'])) ? $res['title'] : '' ?></h5>
+                    <p class="card-text mb-5 ms-1"><?php echo $res['content'] ?></p>
+                    <form method="post" action="home.php">
+                            <input type="hidden" name="user_id" value="<?php echo $res['user_id'] ?>">
+                            <input type="hidden" name="post_id" value="<?php echo $res['post_id'] ?>">
+                            <input type="hidden" name="like_value" value="<?php echo $res['likes'] ?>">
+                        <button class="btn btn-light" type="submit" name="like_btn"><?php echo $res['likes'] ?> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></button>
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <h6>Uploaded by <?php echo $res['username'] ?></h6>
                 </div>
             </div>
 
+            <?php
 
+            }
 
+            ?>
 
         </div>
 
@@ -90,12 +140,25 @@ if (isset($_POST['post_button'])){
 
             <div class="card">
                 <div class="card-header">
-                    <h6>My Profile</h6>
+                    <h6>About Me</h6>
                 </div>
                 <div class="card-body">
-
-
-
+                    <div class="table-responsive">
+                        <table class="table">
+                            <tr>
+                                <td>Full Name</td>
+                                <td><?php echo $row['firstname'] . ' ' .$row['lastname'] ?></td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td><?php echo $row['email'] ?></td>
+                            </tr>
+                            <tr>
+                                <td>Username</td>
+                                <td><?php echo $row['username'] ?></td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
 
@@ -103,12 +166,6 @@ if (isset($_POST['post_button'])){
     </div>
 
 </div>
-
-
-
-
-
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,10 +5,36 @@
     if (isset($_POST['login_btn'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
-
         $sql = "SELECT * FROM users WHERE `username` = '$username'";
         $result = mysqli_query($con, $sql);
+        $row = mysqli_fetch_assoc($result);
+        if (mysqli_num_rows($result) > 0){
+            if ($row['password'] == $password){
+                $_SESSION['isLoggedIn'] = true;
+                $_SESSION['id'] = $row['user_id'];
+                header("Location: home.php");
+            } else {
+                echo "Incorrect password";
+            }
+        } else {
+            echo "No user found";
+        }
+    }
 
+    if (isset($_POST['register_btn'])){
+
+        $firstname = $_POST['fname'];
+        $lastname = $_POST['lname'];
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $sql = "INSERT INTO `users` (firstname, lastname, email, username, password) VALUES ('$firstname', '$lastname', '$email', '$username', '$password')";
+        $result = mysqli_query($con, $sql);
+
+        if ($result === TRUE){
+            echo "Register successful! You may now login";
+        }
 
     }
 
@@ -20,7 +46,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Login | WeShare</title>
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
@@ -30,7 +56,7 @@
             <div class="imgBx"><img src="https://raw.githubusercontent.com/WoojinFive/CSS_Playground/master/Responsive%20Login%20and%20Registration%20Form/img1.jpg" alt="" /></div>
             <div class="formBx">
                 <form action="index.php" method="post">
-                    <h2>Sign In</h2>
+                    <h2>Sign In to WeShare</h2>
                     <input type="text" name="username" placeholder="Username" />
                     <input type="password" name="password" placeholder="Password" />
                     <input type="submit" name="login_btn" value="Login" />
@@ -43,13 +69,14 @@
         </div>
         <div class="user signupBx">
             <div class="formBx">
-                <form action="" onsubmit="return false;">
+                <form action="index.php" method="post">
                     <h2>Create an account</h2>
-                    <input type="text" name="" placeholder="Username" />
-                    <input type="email" name="" placeholder="Email Address" />
-                    <input type="password" name="" placeholder="Create Password" />
-                    <input type="password" name="" placeholder="Confirm Password" />
-                    <input type="submit" name="" value="Sign Up" />
+                    <input type="text" name="fname" placeholder="First Name" />
+                    <input type="text" name="lname" placeholder="Last Name" />
+                    <input type="text" name="uname" placeholder="Username" />
+                    <input type="email" name="email" placeholder="Email Address" />
+                    <input type="text" name="password" placeholder="Create Password" />
+                    <input type="submit" name="register_btn" value="Sign Up" />
                     <p class="signup">
                         Already have an account ?
                         <a href="#" onclick="toggleForm();">Sign in.</a>
